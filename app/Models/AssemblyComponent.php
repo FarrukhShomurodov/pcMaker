1aafd1cc85a6809aa2ece93c54282074848f4539
FarrukhShomurodov <uamerike@gmail.com>
2024-09-22 20:53:27 +0500

    test bot

--- app/Models/AssemblyComponent.php
+++ app/Models/AssemblyComponent.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AssemblyComponent extends Model
 {
@@ -13,4 +14,14 @@ class AssemblyComponent extends Model
         'component_id',
         'assembly_id',
     ];
+
+    public function component(): BelongsTo
+    {
+        return $this->belongsTo(Component::class, 'component_id');
+    }
+
+    public function assembly(): BelongsTo
+    {
+        return $this->belongsTo(Assembly::class, 'assembly_id');
+    }
 }
