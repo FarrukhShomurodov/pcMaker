a7ef1ec8c0d649b04b3be747fa66c3604fddab9c
FarrukhShomurodov <uamerike@gmail.com>
2024-09-22 20:33:37 +0500

    test bot

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -86,7 +86,7 @@ class TelegramService
             case '🔧 Комплектующие':
                 $this->showAdminCategory($chatId);
                 break;
-//            default:
+            default:
 //                if ($step === 'show_main_menu' || $step === 'show_subcategory') {
 //                    $this->checkCategory($chatId, $text);
 //                } elseif ($step === 'select_category') {
@@ -96,34 +96,33 @@ class TelegramService
 //                } else {
 //                    $this->showMainMenu($chatId);
 //                }
-//                break;
-        }
-
-        switch ($step) {
-            case 'choose_language':
-                $this->processLanguageChoice($chatId, $text);
-                break;
-            case 'request_phone':
-                $this->processPhoneRequest($chatId, $message);
-                break;
-            case 'confirm_phone':
-                $this->processPhoneConfirmation($chatId, $text);
-                break;
-            case 'request_name':
-                $this->processNameRequest($chatId, $text);
-                break;
-            case 'select_category':
-                $this->selectCategory($chatId, $text);
-                break;
-            case 'select_component':
-                $this->selectComponent($chatId, $text);
-                break;
+                switch ($step) {
+                    case 'choose_language':
+                        $this->processLanguageChoice($chatId, $text);
+                        break;
+                    case 'request_phone':
+                        $this->processPhoneRequest($chatId, $message);
+                        break;
+                    case 'confirm_phone':
+                        $this->processPhoneConfirmation($chatId, $text);
+                        break;
+                    case 'request_name':
+                        $this->processNameRequest($chatId, $text);
+                        break;
+                    case 'select_category':
+                        $this->selectCategory($chatId, $text);
+                        break;
+                    case 'select_component':
+                        $this->selectComponent($chatId, $text);
+                        break;
 //            case 'show_main_menu':
 //                $this->showMainMenu($chatId);
 //                break;
 //            default:
 //                $this->showMainMenu($chatId);
 //                break;
+                }
+                break;
         }
     }
 
