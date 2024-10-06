73d963db7ef233a876f1e571478f69c00ec076a9
FarrukhShomurodov <uamerike@gmail.com>
2024-10-06 21:31:28 +0500

    admin panel

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/sub-categories/{productCategory}', [SubCategoryController::class, 'fetchByCategory']);
-Route::get('/api/orders/show/{order}', [OrderController::class, 'show']);
+Route::get('/orders/show/{order}', [OrderController::class, 'show']);
